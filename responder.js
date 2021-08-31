9d069f312150a83dbfd8bdb46f03cee0119dabe8
s1ckkk <89815245+s1ckkk@users.noreply.github.com>
2021-08-31 20:15:54 +0500

    Add files via upload

---
+++ responder.js
@@ -0,0 +1,29 @@
+
+const Discord = require('discord.js-selfbot');
+const client = new Discord.Client();
+
+
+   
+
+client.on('ready', () => {
+    //client.channels.cache.get(`630995793670701065`).send(`Ready...`)
+    console.log(`Logged in as ${client.user.tag}!`);
+    //	client.channels.cache.get(`630995793670701065`).send(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity(`.gg/chillvibe`);
+    client.user.setStatus('discord.gg/chillvibe')
+});
+
+
+client.on('message', message => {
+    if (message.content === '<@792736368476291132>') {
+        message.channel.send('!d bump');
+    }
+});
+client.on("message", message => {
+
+    if (message.mentions.has(client.user.id)) {
+        message.channel.send("!d bump");
+    };
+});
+
+client.login("NzkyNzM2MzY4NDc2MjkxMTMy.YMOfeQ.akkvLRfHVdZZxoxUHXgtT6oYpJ0");
